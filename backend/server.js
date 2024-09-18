d8e1913001ab309837091d8f390a651f508cbd39
talha110-cyber <129695603+talha110-cyber@users.noreply.github.com>
2024-09-18 22:53:32 +0500

    a

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,14 @@ const app = express();
 app.use(cors());  // Add this line to enable CORS
 
 // Middleware to log IP address of incoming requests
+const requestIp = require('request-ip');
+
+app.use(requestIp.mw());
+
 app.use((req, res, next) => {
-    console.log(`Client IP: ${req.ip}`);
+    console.log(`Client IP: ${req.clientIp}`);
     next();
 });
-
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
@@ -26,6 +29,6 @@ app.get('*', (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
