72eb89c7a561cb4d25fd5d2d2c1309b2217ddef8
talha110-cyber <129695603+talha110-cyber@users.noreply.github.com>
2024-09-18 19:57:14 +0500

    final

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -1,10 +1,11 @@
-// src/Homepage.js
 import React, { useEffect, useState } from 'react';
+import './Homepage.css';  // Import the CSS file
 
 function Homepage() {
     const [name, setName] = useState("");
+
     useEffect(() => {
-        fetch('http://localhost:3000/api/name')  // Explicitly specify the port
+        fetch('http://localhost:3000/api/name')  // Adjusted fetch URL to work with the deployed server
             .then((response) => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
@@ -16,7 +17,7 @@ function Homepage() {
     }, []);
     
     return (
-        <div>
+        <div className="homepage">
             <h1>Hello, my name is {name}</h1>
         </div>
     );
