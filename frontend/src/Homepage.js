bf89bd15a07fae959848c3439c2d8a6a9f7b0f91
talha110-cyber <129695603+talha110-cyber@users.noreply.github.com>
2024-09-26 03:14:31 +0500

    a

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -13,7 +13,7 @@ function Homepage() {
                 setIp(fetchedIp);
 
                 // Send the IP address to the server
-                const serverUrl = 'http://13.49.230.69:5000/api/receive-ip'; // Update to your backend URL
+                const serverUrl = '/api/receive-ip'; // Use relative path for Vercel
                 axios.post(serverUrl, { ip: fetchedIp }, {
                     headers: {
                         'Content-Type': 'application/json',
@@ -38,7 +38,7 @@ function Homepage() {
         <div className="homepage">
             <h1>Welcome!</h1>
             <p className="ip-display">Your IP is: {ip ? ip : 'Loading...'}</p>
-            <p>soon we will tell you more about yourselves :) </p>
+            <p>Soon we will tell you more about yourselves :)</p>
         </div>
     );
 }
